fbffa37da7a49b66cd0940e5af1ecfd6238179be
pnhung177 <pnhung177@gmail.com>
2023-06-02 17:40:23 +0700

    Add the moving (up, down, left, right) functions that supports the D-Pad buttons

--- Rotatable-Pedestal/Rotatable-Pedestal.ino
+++ Rotatable-Pedestal/Rotatable-Pedestal.ino
@@ -24,7 +24,7 @@ void setup() {
 void loop() {
   testServos();
   //
-  if(error == 1) { //skip loop if no controller found
+  if(type != 1) { //skip loop if no controller found
     Serial.println("Error, terminated!");
     return;
   } else { //DualShock Controller
@@ -35,27 +35,85 @@ void loop() {
     // MOVE FORWARD
     if(ps2x.Button(PSB_PAD_UP)) {
       Serial.println("PSB_PAD_UP is pushed");
+      verticalServoUp();
     }
     // MOVE BACK
     if(ps2x.Button(PSB_PAD_DOWN)){
-      Serial.println("PSB_PAD_UP is pushed");
+      Serial.println("PSB_PAD_DOWN is pushed");
+      verticalServoDown();
     }
     // TURN LEFT
     if(ps2x.Button(PSB_PAD_LEFT)){
       Serial.println("PSB_PAD_LEFT is pushed");
+      horizontalServoLeft();
     }
     // TURN RIGHT
     if(ps2x.Button(PSB_PAD_RIGHT)){
       Serial.println("PSB_PAD_RIGHT is pushed");
+      horizontalServoRight();
     }
   }
   //
   delay(500);
 }
 
+bool verticalServoUp() {
+  int vPos = verticalServo.read();
+  if (vPos >= 180) {
+    return false;
+  }
+  vPos += 5;
+  if (vPos > 180) {
+    vPos = 180;
+  }
+  verticalServo.write(vPos);
+  return true;
+}
+
+bool verticalServoDown() {
+  int vPos = verticalServo.read();
+  if (vPos <= 0) {
+    return false;
+  }
+  vPos -= 5;
+  if (vPos < 0) {
+    vPos = 0;
+  }
+  verticalServo.write(vPos);
+  return true;
+}
+
+bool horizontalServoLeft() {
+  int hPos = horizontalServo.read();
+  if (hPos >= 180) {
+    return false;
+  }
+  hPos += 5;
+  if (hPos > 180) {
+    hPos = 180;
+  }
+  horizontalServo.write(hPos);
+  return true;
+}
+
+bool horizontalServoRight() {
+  int hPos = horizontalServo.read();
+  if (hPos <= 0) {
+    return false;
+  }
+  hPos -= 5;
+  if (hPos < 0) {
+    hPos = 0;
+  }
+  horizontalServo.write(hPos);
+  return true;
+}
+
 void initServos() {
   horizontalServo.attach(HORIZONTAL_SERVO_PIN);
+  horizontalServo.write(0);
   verticalServo.attach(VERTICAL_SERVO_PIN);
+  verticalServo.write(0);
 }
 
 void testServos() {
